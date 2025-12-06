0756f8d89e812d97c806a2a6c8b7067bdcd35f5b
dalfonsop <dalfonsop94@gmail.com>
2025-12-06 12:01:52 -0500

    BuilLeading & BuildActions

--- lib/presentation/delegates/search_movie_delegate.dart
+++ lib/presentation/delegates/search_movie_delegate.dart
@@ -1,31 +1,37 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
 
-class SearchMovieDelegate extends SearchDelegate{
-
+class SearchMovieDelegate extends SearchDelegate<Movie?> {
   @override
   String? get searchFieldLabel => 'Buscar película';
 
-
   @override
   List<Widget>? buildActions(BuildContext context) {
-    return [Text('BuildActions')];
+    return [
+      FadeIn(
+        duration: Duration(milliseconds: 200),
+        animate: query.isNotEmpty,
+        child: IconButton(onPressed: () => query = '', icon: Icon(Icons.clear)),
+      ),
+    ];
   }
 
   @override
   Widget? buildLeading(BuildContext context) {
-    return Text('BuildLeading');
+    return IconButton(
+      onPressed: () => close(context, null),
+      icon: Icon(Icons.arrow_back),
+    );
   }
 
   @override
   Widget buildResults(BuildContext context) {
     return Text('buildResults');
-
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
     return Text('buildSuggestions');
-
   }
-
-}  
+}
